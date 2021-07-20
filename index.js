366e784d684da7af13c7f896bdc7d1eaf12c09d4
PratyushK7 <pratyushnjr9+github@gmail.com>
2021-07-21 00:55:13 +0530

    Test Build

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // IMPORTS
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const userRoute = require("./routes/users");
-const authRoute = require("./routes/auth");
-const postRoute = require("./routes/posts");
-const multer = require("multer");
-const path = require("path");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+const multer = require('multer');
+const path = require('path');
 
 // DOTENV CONFIGURED
 dotenv.config();
@@ -19,20 +19,20 @@ mongoose.connect(
   process.env.MONGO_URL,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   }
 );
 
-app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 // MIDDLEWARES
 app.use(express.json());
 app.use(helmet());
-app.use(morgan("common"));
+app.use(morgan('common'));
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/images");
+    cb(null, 'public/images');
   },
   filename: function (req, file, cb) {
     cb(null, req.body.name);
@@ -40,19 +40,28 @@ var storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
+app.post('/api/upload', upload.single('file'), (req, res) => {
   try {
-    return res.status(200).json("File uploded successfully");
+    return res.status(200).json('File uploded successfully');
   } catch (error) {
     console.error(error);
   }
 });
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
+
+app.get('/', (req, res) => {
+  app.use(
+    express.static(path.resolve(__dirname, 'react-tailwind-ui', 'build'))
+  );
+  res.sendFile(
+    path.resolve(__dirname, 'react-tailwind-ui', 'build', 'index.html')
+  );
+});
 
 // BACKEND SEREVER PORT
 app.listen(8800, () => {
-  console.log("Backend server is running!");
+  console.log('Backend server is running!');
 });
